141249de8ea3d4ceac0690354e619994205fef5e
Meyvi <meyvim@gmail.com>
2020-06-21 19:27:28 -0700

    Implemented isEven()

--- src/recursion.js
+++ src/recursion.js
@@ -58,6 +58,17 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n < 0) {
+    n = n * -1;
+  };
+  //base case
+    if (n === 1) {
+      return false;
+    };
+    if (n === 0) {
+      return true;
+    };
+    return isEven(n-2);
 };
 
 // 5. Sum all integers below a given integer.
