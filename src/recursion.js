8e16d240636234bc3078abdf3c8943d7da42a504
Meyvi <meyvim@gmail.com>
2020-06-21 18:59:40 -0700

    Implemented sum of array

--- src/recursion.js
+++ src/recursion.js
@@ -27,11 +27,21 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+//basecase
+  if (array.length === 1) {
+    return array[0];
+  }
+  if (array.length < 1) {
+    return 0;
+  }
+//recursive case
+  if (array.length > 1) {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
 };
 
 // 4. Check if a number is even.
